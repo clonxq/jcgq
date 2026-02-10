3121ac160b9c78b16c95cf4283c7d1853f33e5dc
xqrock <58764635+xqrock@users.noreply.github.com>
2026-02-11 00:10:40 +0800

    Update 2410part2.sh

--- 2410part2.sh
+++ 2410part2.sh
@@ -40,26 +40,3 @@ if [ -f "$RUST_FILE" ]; then
 
 	cd $PKG_PATH && echo "rust has been fixed!"
 fi
-
-
-# --- 新增：修复源码底层配置错误 ---
-
-# 3. 修复 rd05a1 递归依赖错误 (必须执行，否则 config 不准)
-find feeds/ -name "rd05a1" -type d | xargs rm -rf
-
-# 4. 修复 usbgadget 警告
-IFILE="package/utils/usbgadget/Makefile"
-[ -f "$IFILE" ] && sed -i 's/+kmod-usb-gadget-ncm//g' "$IFILE"
-
-# 5. 【核心步骤】彻底拦截并引诱 Rust 报错
-# 删除 Rust 源码文件夹。如果编译依然需要 Rust，
-# 报错信息将不再是 "checksum 错误"，而是 "No rule to make target ... rust"
-# 在那行错误的上方，会显示： "make[3]: Entering directory '/workdir/openwrt/feeds/packages/xxxx'"
-# 那个 xxxx 就是真正的元凶！
-rm -rf feeds/packages/lang/rust
-
-# 6. 在 .config 中显式禁用 Rust 相关项（尝试拦截隐式选中）
-sed -i '/CONFIG_PACKAGE_rust=y/d' .config
-sed -i '/CONFIG_PACKAGE_librust=y/d' .config
-echo "CONFIG_PACKAGE_rust=n" >> .config
-echo "CONFIG_PACKAGE_librust=n" >> .config
