4bc7c2511663065f67ee5d7323d971688e5c278b
clonxq <164881770+clonxq@users.noreply.github.com>
2026-02-19 12:10:07 +0800

    Update 2410part1.sh

--- 2410part1.sh
+++ 2410part1.sh
@@ -44,4 +44,3 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
-extract_pkg "https://github.com/kenzok8/openwrt-packages" "adguardhome" "package/adguardhome"
