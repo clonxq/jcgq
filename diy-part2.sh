c7c0903f4aae486153c7b8bdfe6ee54f290370a3
clonxq <164881770+clonxq@users.noreply.github.com>
2026-02-21 21:34:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,9 @@ rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-passwal2
 rm -rf ./feeds/luci/applications/luci-app-alist
-# rm -rf ./feeds/luci/applications/luci-app-mwan3helper
+rm -rf ./feeds/luci/applications/luci-app-mwan3helper
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf ./feeds/luci/applications/luci-app-openclash
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
