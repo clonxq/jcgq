796cf281fab61cbb27e5a1d9246a030800bf9e3c
xqrock <58764635+xqrock@users.noreply.github.com>
2026-03-09 08:37:48 +0800

    Update 2410part2.sh

--- 2410part2.sh
+++ 2410part2.sh
@@ -24,6 +24,7 @@ rm -rf ./feeds/packages/net/shadowsocksr-libev
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-passwall2
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf ./feeds/luci/applications/luci-app-openclash
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
